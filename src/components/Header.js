a912b6bea0de61f5f9d9a4389a262ce108c481f4
Fellipe Souza <ofellipe2023@gmail.com>
2024-09-19 17:43:11 -0300

    responsividade

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../style/Header.css'; // Arquivo de estilo CSS opcional
 import logo from '../img/logo_mbr.png';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
       <div className="top-bar">
-      <p className='endereco'>
-        <i className="fa fa-map-marker"></i> Rua Honduras, 25 - Penha, RJ
-      </p> 
+        <p className='endereco'>
+          <i className="fa fa-map-marker"></i> Rua Honduras, 25 - Penha, RJ
+        </p> 
         <div className="social">
           <a rel="nofollow" className="social-icons" href="mailto:ofellipe2023@gmail.com" target="_blank" title="Envie um E-mail">
             <i className="fa fa-envelope fa-x3"></i>
@@ -21,12 +27,18 @@ const Header = () => {
           <p>(21) 96841-1741</p>
           <p>3654-3128 </p>
         </div>
-
       </div>
+      
       <div className="main-header">
         <img src={logo} alt="Logo" className="header-logo" />
-        {/* <h1>MBR II</h1> */}
-        <nav>
+        
+        {/* Ícone do menu hamburger */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          <i className={isMenuOpen ? "fa fa-times" : "fa fa-bars"}></i>
+        </div>
+        
+        {/* Menu de navegação condicional */}
+        <nav className={isMenuOpen ? "nav-menu open" : "nav-menu"}>
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/quem-somos">Quem Somos</Link></li>
