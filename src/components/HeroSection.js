28a853a63c1b174c1efafde3be7c61ca1c1685d7
Fellipe Souza <ofellipe2023@gmail.com>
2024-09-23 13:36:09 -0300

    carousel funcionando

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,8 @@
-// src/components/HeroSection.js
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Caso ainda não tenha importado o Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa o Bootstrap
 import '../style/HeroSection.css'; // Estilo personalizado
+import banner1 from '../img/banner1.png';
+import banner2 from '../img/banner2.png';
 
 const HeroSection = () => {
   return (
@@ -9,20 +10,20 @@ const HeroSection = () => {
       <div id="heroCarousel" className="carousel slide" data-bs-ride="carousel">
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src="https://via.placeholder.com/1200x500" className="d-block w-100" alt="First slide" />
+            <img src={banner1} className="d-block w-100" alt="First slide" />
           </div>
           <div className="carousel-item">
-            <img src="https://via.placeholder.com/1200x500" className="d-block w-100" alt="Second slide" />
+            <img src={banner2} className="d-block w-100" alt="Second slide" />
           </div>
         </div>
-        <a className="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <button className="carousel-control-prev" type="button" data-bs-target="#heroCarousel" data-bs-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Anterior</span>
-        </a>
-        <a className="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#heroCarousel" data-bs-slide="next">
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Próximo</span>
-        </a>
+        </button>
       </div>
 
       <div className="text-center my-5 sectionDois">
@@ -73,19 +74,17 @@ const HeroSection = () => {
           <h2>SOBRE A MBR</h2>
           <p>Especializada na coleta e reciclagem.</p>
         </div>
-          <div className="info-section">
-          <div className="info-container">
-            <div className="info-content">
-              <h1>Aqui preservamos o meio ambiente</h1>
-              <p>A MBR está equipada para o tratamento de óleos e gorduras vegetais e os resíduos gerados pelo processo de reciclagem, garantindo 100% de aproveitamento.As coletas são feitas em estabelecimentos comerciais: restaurantes, padarias, cozinhas industriais, indústrias alimentícias, residências, além de pontos de coletas distribuídos em supermercados em toda Baixada Santista.O óleo tratado é enviado para a indústria de biodiesel.</p>
-              <div className="circle">
-                <div className="circle-content">
-                  <span className="circle-text">100%</span>
-                </div>
-                <div className="circle-description">
-                  <h6>MISSÃO</h6>
-                  <p>Conscientizar a população sobre a necessidade de preservar o meio ambiente e a água.</p>
-                </div>
+        <div className="info-container">
+          <div className="info-content">
+            <h1>Aqui preservamos o meio ambiente</h1>
+            <p>A MBR está equipada para o tratamento de óleos e gorduras vegetais e os resíduos gerados pelo processo de reciclagem, garantindo 100% de aproveitamento. As coletas são feitas em estabelecimentos comerciais: restaurantes, padarias, cozinhas industriais, indústrias alimentícias, residências, além de pontos de coletas distribuídos em supermercados em toda Baixada Santista. O óleo tratado é enviado para a indústria de biodiesel.</p>
+            <div className="circle">
+              <div className="circle-content">
+                <span className="circle-text">100%</span>
+              </div>
+              <div className="circle-description">
+                <h6>MISSÃO</h6>
+                <p>Conscientizar a população sobre a necessidade de preservar o meio ambiente e a água.</p>
               </div>
             </div>
           </div>
@@ -93,37 +92,36 @@ const HeroSection = () => {
       </div>
 
       <div className='cuideDaNat'>
-          <h3>Cuide da Natureza</h3>
-          <p>Pequenas ações hoje vão garantir grandes resultados no futuro.</p>
+        <h3>Cuide da Natureza</h3>
+        <p>Pequenas ações hoje vão garantir grandes resultados no futuro.</p>
 
-          <div className='image-gallery'>
-            <div className='image-item'>
-              <img src='https://www.biolitoral.com.br/imagens/cuidar-meio-ambiente.png' alt='Imagem 1' className='img-circle' />
-              <h6>CUIDAR DO MEIO AMBIENTE</h6>
-              <hr></hr>
-              <p>Preservar é qualidade de vida.</p>
-            </div>
-            <div className='image-item'>
-              <img src='https://www.biolitoral.com.br/imagens/cuide-futuro.png' alt='Imagem 2' className='img-circle' />
-              <h6>CUIDE DO FUTURO</h6>
-              <hr></hr>
-              <p>O futuro é o “hoje”.</p>
-            </div>
-            <div className='image-item'>
-              <img src='https://www.biolitoral.com.br/imagens/pequenos-gestos.png' alt='Imagem 3' className='img-circle' />
-              <h6>PEQUENOS GESTOS</h6>
-              <hr></hr>
-              <p>Geram grandes resultados.</p>
-            </div>
-            <div className='image-item'>
-              <img src='https://www.biolitoral.com.br/imagens/reciclar.png' alt='Imagem 4' className='img-circle' />
-              <h6>RECICLAR</h6>
-              <hr></hr>
-              <p>Esse é o caminho</p>
-            </div>
+        <div className='image-gallery'>
+          <div className='image-item'>
+            <img src='https://www.biolitoral.com.br/imagens/cuidar-meio-ambiente.png' alt='Imagem 1' className='img-circle' />
+            <h6>CUIDAR DO MEIO AMBIENTE</h6>
+            <hr />
+            <p>Preservar é qualidade de vida.</p>
+          </div>
+          <div className='image-item'>
+            <img src='https://www.biolitoral.com.br/imagens/cuide-futuro.png' alt='Imagem 2' className='img-circle' />
+            <h6>CUIDE DO FUTURO</h6>
+            <hr />
+            <p>O futuro é o “hoje”.</p>
+          </div>
+          <div className='image-item'>
+            <img src='https://www.biolitoral.com.br/imagens/pequenos-gestos.png' alt='Imagem 3' className='img-circle' />
+            <h6>PEQUENOS GESTOS</h6>
+            <hr />
+            <p>Geram grandes resultados.</p>
+          </div>
+          <div className='image-item'>
+            <img src='https://www.biolitoral.com.br/imagens/reciclar.png' alt='Imagem 4' className='img-circle' />
+            <h6>RECICLAR</h6>
+            <hr />
+            <p>Esse é o caminho</p>
           </div>
         </div>
-
+      </div>
     </div>
   );
 };
